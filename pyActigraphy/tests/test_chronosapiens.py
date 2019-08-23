f97588372de86078d7ce49cf9501865f0c8bd389
Grégory Hammad <gregory.hammad@hotmail.fr>
2019-08-23 10:28:27 +0200

    Add first test for Chronosapiens

--- pyActigraphy/tests/test_chronosapiens.py
+++ pyActigraphy/tests/test_chronosapiens.py
@@ -1,11 +1,11 @@
-from generate_dataset import generate_series
-from generate_dataset import generate_sinewave
-from generate_dataset import generate_squarewave
-
 import numpy as np
 import pandas as pd
 import pyActigraphy
-# from pytest import approx
+
+from pytest import approx
+from generate_dataset import generate_series
+from generate_dataset import generate_sinewave
+
 
 sampling_period = 60
 frequency = pd.Timedelta(sampling_period, unit='s')
@@ -13,11 +13,6 @@ start_time = '2018/01/01 08:00:00'
 N = 20160
 period = pd.Timedelta(N, unit='T')
 
-square_wave = generate_series(
-    generate_squarewave(N=N, offset=True),
-    start=start_time,
-    sampling_period=sampling_period
-)
 sine_wave = generate_series(
     generate_sinewave(N=N, offset=True),
     start=start_time,
@@ -38,25 +33,14 @@ n_days = int(period/pd.Timedelta('1D'))-1
 
 # Create associated sleep signal (awake:0, sleep:1)
 sleep_wave = generate_series(
-    # generate_squarewave(N=N-1, A=1, offset=False),
     n_epochs_nan_trend*[np.nan] +
     (n_epochs_awake_prior_sleep*[0] +
      n_epochs_asleep*[1] +
      n_epochs_awake_after_sleep*[0])*n_days +
-    n_epochs_nan_trend*[np.nan],
+    [0] + (n_epochs_nan_trend-1)*[np.nan],  # At midnight on the last day,
+    # there are still enough data left to calculate the trend.
     start=start_time,
     sampling_period=sampling_period
-)
-raw_squarewave = pyActigraphy.io.BaseRaw(
-    name='raw_square',
-    uuid='XXXXXXXX',
-    format='CUSTOM',
-    axial_mode=None,
-    start_time=pd.to_datetime(start_time),
-    period=period,
-    frequency=frequency,
-    data=square_wave,
-    light=None
 )
 raw_sinewave = pyActigraphy.io.BaseRaw(
     name='raw_sinewave',
@@ -89,12 +73,11 @@ raw_sleepwave = pyActigraphy.io.BaseRaw(
 
 def test_chronosapiens_sinewave():
 
-    assert pd.testing.assert_series_equal(
-        raw_sinewave.Chronosapiens(threshold=0.5, min_trend_period='24h'),
-        raw_sleepwave.data
+    test = raw_sinewave.Chronosapiens(threshold=0.5, min_trend_period='24h')
+    index_equality = np.alltrue(raw_sleepwave.data.index == test.index)
+    value_equality = np.isclose(raw_sleepwave.data, test, equal_nan=True)
+
+    assert (
+        index_equality and
+        np.sum(value_equality)/len(value_equality) == approx(1, rel=0.01)
     )
-
-
-# def test_rar_ra_squarewave():
-#
-#     assert raw_squarewave.RA(binarize=True) == approx(1.0)
