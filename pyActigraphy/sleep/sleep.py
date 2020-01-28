d2eb3fd8c572bafddd3fff792f990d5eb5dfc3de
Grégory Hammad <gregory.hammad@hotmail.fr>
2020-01-28 21:48:23 +0100

    Add min/max duration in sleep bout detection. Add duration

--- pyActigraphy/sleep/sleep.py
+++ pyActigraphy/sleep/sleep.py
@@ -1,21 +1,35 @@
-# from ..io import BaseRaw, RawReader
-# from ..io.base import BaseRaw
+from ..utils.filters import filter_ts_duration
 
 
 class SleepBoutMixin(object):
     """ Mixin Class for identifying sleep bouts"""
 
-    def sleep_bouts(self, method='crespo'):
+    def sleep_bouts(
+        self,
+        duration_min=None,
+        duration_max=None,
+        algo='Roenneberg',
+        *args, **kwargs
+    ):
         r"""Sleep bouts.
 
         Activity periods identified as sleep.
 
         Parameters
         ----------
-        method: str
-            Method used to identify continuous sleep bouts.
-            Available methods are: 'crespo', 'roenneberg'
-            Default is 'crespo'.
+        duration_min: str,optional
+            Minimal time duration for a sleep period.
+            Default is None (no filtering).
+        duration_max: str,optional
+            Maximal time duration for a sleep period.
+            Default is None (no filtering).
+        algo: str, optional
+            Sleep scoring algorithm to use.
+            Default is 'Roenneberg'.
+        *args
+            Variable length argument list passed to the scoring algorithm.
+        **kwargs
+            Arbitrary keyword arguements passed to the scoring algorithm.
 
         Returns
         -------
@@ -27,34 +41,73 @@ class SleepBoutMixin(object):
 
             >>> import pyActigraphy
             >>> rawAWD = pyActigraphy.io.read_raw_awd(fpath + 'SUBJECT_01.AWD')
-            >>> raw.sleep_bouts()
+            >>> raw.sleep_bouts(duration_min='2h', algo='Roenneberg')
             XXX
 
         """
 
-        methods = ('crespo', 'roenneberg')
-        if method not in methods:
-            raise ValueError(
-                '`method` must be "%s". You passed: "%s"' %
-                ('" or "'.join(methods), method)
-            )
+        # Retrieve sleep scoring function dynamically by name
+        sleep_algo = getattr(self, algo+'_AoT')
 
-        # Create a RawReader instance
-        sleep_bouts = []  # RawReader(reader_type)
-
-        # Identify sleep bouts
-        if method == 'crespo':
-            # retrieve activity onset and offset times
-            onsets, offsets = self.Crespo_AoT()
-        elif method == 'roenneberg':
-            # retrieve activity onset and offset times
-            onsets, offsets = self.Roenneberg_AoT()
+        # Detect activity onset and offset times
+        onsets, offsets = sleep_algo(*args, **kwargs)
 
         # For each inactivity period (from offset to onset times)
-        # create a BaseRaw object
+        sleep_bouts = []
         for onset, offset in zip(onsets, offsets):
             sleep_bout = self.data[offset:onset]
-
             sleep_bouts.append(sleep_bout)
 
-        return sleep_bouts
+        return filter_ts_duration(sleep_bouts, duration_min, duration_max)
+
+    def sleep_durations(
+        self,
+        duration_min=None,
+        duration_max=None,
+        algo='Roenneberg',
+        *args, **kwargs
+    ):
+        r"""Duration of the sleep bouts.
+
+        Duration of the activity periods identified as sleep.
+
+        Parameters
+        ----------
+        duration_min: str,optional
+            Minimal time duration for a sleep period.
+            Default is None (no filtering).
+        duration_max: str,optional
+            Maximal time duration for a sleep period.
+            Default is None (no filtering).
+        algo: str, optional
+            Sleep scoring algorithm to use.
+            Default is 'Roenneberg'.
+        *args
+            Variable length argument list passed to the scoring algorithm.
+        **kwargs
+            Arbitrary keyword arguements passed to the scoring algorithm.
+
+        Returns
+        -------
+        sleep_durations: a list of pandas.TimeDelta
+
+
+        Examples
+        --------
+
+            >>> import pyActigraphy
+            >>> rawAWD = pyActigraphy.io.read_raw_awd(fpath + 'SUBJECT_01.AWD')
+            >>> raw.sleep_durations(duration_min='2h', algo='Roenneberg')
+            XXX
+
+        """
+
+        # Retrieve sleep bouts
+        filtered_bouts = self.sleep_bouts(
+            duration_min=duration_min,
+            duration_max=duration_max,
+            algo=algo,
+            *args, **kwargs
+        )
+
+        return [s.index[-1]-s.index[0] for s in filtered_bouts]
