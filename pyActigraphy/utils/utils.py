f115fff77f2a23de8d8751f8f42c168fb82863c4
Grégory Hammad <gregory.hammad@hotmail.fr>
2020-01-29 14:38:44 +0100

    Decouple onset detection and daily prof. Export to utils

--- pyActigraphy/utils/utils.py
+++ pyActigraphy/utils/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -29,3 +30,30 @@ def _average_daily_activity(data, cyclic=False):
     avgdaily.index = index
 
     return avgdaily
+
+
+def _onset_detection(x, whs):
+    return np.mean(x[whs:])/np.mean(x[0:whs])-1
+
+
+def _offset_detection(x, whs):
+    return np.mean(x[0:whs])/np.mean(x[whs:])-1
+
+
+def _activity_inflexion_time(data, fct, whs):
+
+    r = data.rolling(whs*2, center=True)
+
+    aot = r.apply(fct, kwargs={'whs': whs}, raw=True).idxmax()
+
+    return aot
+
+
+def _activity_onset_time(data, whs=4):
+
+    return _activity_inflexion_time(data, _onset_detection, whs)
+
+
+def _activity_offset_time(data, whs=4):
+
+    return _activity_inflexion_time(data, _offset_detection, whs)
