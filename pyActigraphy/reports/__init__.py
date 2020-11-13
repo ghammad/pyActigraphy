5c7837162abbf5c10b0ff85ae449e8f3529d18b9
Grégory Hammad <gregory.hammad@hotmail.fr>
2020-11-13 16:13:53 +0100

    Implement base and activity report classes.

---
+++ pyActigraphy/reports/__init__.py
@@ -0,0 +1,10 @@
+"""Classes for activity and sleep reports"""
+
+# Authors: Grégory Hammad <gregory.hammad@uliege.be>
+#
+# License: BSD (3-clause)
+
+# from . import filters
+from .report import Reports, ActivityReports
+
+__all__ = ["Reports", "ActivityReports"]
