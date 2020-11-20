e9885fd719c19c0c995d9d553d8f00393c29f9e9
Grégory Hammad <gregory.hammad@hotmail.fr>
2020-11-20 13:26:33 +0100

    Add non_overlap_fragments function. Fix d2o function.

--- pyActigraphy/reports/utils.py
+++ pyActigraphy/reports/utils.py
@@ -3,11 +3,11 @@ from ..sleep.scoring.utils import consecutive_values
 
 class ScoringDescriptor():
 
-    def __init__(self, truth, scoring, truth_score=1, sleep_score=1):
-        self.truth = (truth == truth_score).astype(int)
-        self.scoring = (scoring == sleep_score).astype(int)
-        self.truth_score = truth_score
-        self.sleep_score = sleep_score
+    def __init__(self, truth, scoring, truth_target=1, scoring_target=1):
+        self.truth = (truth == truth_target).astype(int)
+        self.scoring = (scoring == scoring_target).astype(int)
+        self.truth_target = truth_target
+        self.scoring_target = scoring_target
 
     def overlap(self, inner=False, cast_to_int=False):
 
@@ -34,10 +34,23 @@ class ScoringDescriptor():
         return [overlap_series.iloc[indices[0]:indices[1]]
                 for indices in fragment_indices]
 
-    def distance_to_overlap(self):
+    def non_overlap_fragments(self, inner=False, min_length=0):
+
+        overlap_series = self.overlap(inner=inner, cast_to_int=True)
+
+        fragment_indices = consecutive_values(
+            overlap_series.values, target=0, min_length=min_length
+        )
+
+        return [overlap_series.iloc[indices[0]:indices[1]]
+                for indices in fragment_indices]
+
+    def distance_to_overlap(self, convert_to_num_min=False):
 
         # Search for the position of the first epoch where truth and
         # scoring overlap
-        pos = (self.truth & self.scores).idxmax()
+        pos = self.overlap(inner=True, cast_to_int=True).idxmax()
 
-        return pos - self.truth.index[0]
+        d = pos - self.truth.index[0]
+
+        return d.total_seconds()/60 if convert_to_num_min else d
