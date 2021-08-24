33af7f075eb4b66307e1cfa3832e038c38ab3e87
Grégory Hammad <gregory.hammad@uliege.be>
2021-08-24 11:22:51 +0200

    Add new tests for RPX (ENG,FR,GER).

--- pyActigraphy/tests/test_rpx.py
+++ pyActigraphy/tests/test_rpx.py
@@ -7,37 +7,123 @@ import pandas as pd
 
 FILE = inspect.getfile(inspect.currentframe())
 data_dir = op.join(op.dirname(op.abspath(FILE)), 'data')
-rpx_path = op.join(data_dir, 'test_sample.csv')
+rpx_path_eng = op.join(data_dir, 'test_sample_rpx_eng.csv')
+rpx_path_fr = op.join(data_dir, 'test_sample_rpx_fr.csv')
+rpx_path_ger = op.join(data_dir, 'test_sample_rpx_ger_no_light.csv')
 
-# read RPX with default parameters
-rawRPX = pyActigraphy.io.read_raw_rpx(rpx_path, language='FR')
+# read RPX ENG with default parameters
+rawRPX_ENG = pyActigraphy.io.read_raw_rpx(
+    rpx_path_eng, language='ENG_UK')
 
+# read RPX FR with default parameters
+rawRPX_FR = pyActigraphy.io.read_raw_rpx(
+    rpx_path_fr, language='FR')
 
-def test_instance_rpx():
+# read RPX FR with modified parameters
+rawRPX_FR_NaN = pyActigraphy.io.read_raw_rpx(
+    rpx_path_fr, language='FR', drop_na=False)
 
-    assert isinstance(rawRPX, pyActigraphy.io.rpx.RawRPX)
-
-
-def test_read_raw_rpx_name():
-
-    assert rawRPX.name == 'TEST_SAMPLE'
-
-
-def test_read_raw_rpx_uuid():
-
-    assert rawRPX.uuid == 'A18272'
-
-
-def test_read_raw_rpx_start_time():
-
-    assert rawRPX.start_time == pd.Timestamp('2015-02-04 11:45:00')
+# read RPX GER with default parameters
+rawRPX_GER = pyActigraphy.io.read_raw_rpx(
+    rpx_path_ger, language='GER', drop_na=False)
 
 
-def test_read_raw_rpx_frequency():
+def test_read_raw_rpx_eng_name():
 
-    assert rawRPX.frequency == pd.Timedelta('00:01:00')
+    assert rawRPX_ENG.name == 'TEST_SAMPLE_UK'
 
 
-def test_read_raw_rpx_data():
+def test_read_raw_rpx_eng_uuid():
 
-    assert len(rawRPX.data) == 11555
+    assert rawRPX_ENG.uuid == 'AXXXUK'
+
+
+def test_read_raw_rpx_eng_start_time():
+
+    assert rawRPX_ENG.start_time == pd.Timestamp('2015-07-04 09:45:00')
+
+
+def test_read_raw_rpx_eng_frequency():
+
+    assert rawRPX_ENG.frequency == pd.Timedelta('00:00:30')
+
+
+def test_instance_rpx_fr():
+
+    assert isinstance(rawRPX_FR, pyActigraphy.io.rpx.RawRPX)
+
+
+def test_read_raw_rpx_fr_name():
+
+    assert rawRPX_FR.name == 'TEST_SAMPLE_FR'
+
+
+def test_read_raw_rpx_fr_uuid():
+
+    assert rawRPX_FR.uuid == 'AXXXFR'
+
+
+def test_read_raw_rpx_fr_start_time():
+
+    assert rawRPX_FR.start_time == pd.Timestamp('2015-02-04 11:45:00')
+
+
+def test_read_raw_rpx_fr_frequency():
+
+    assert rawRPX_FR.frequency == pd.Timedelta('00:01:00')
+
+
+def test_read_raw_rpx_fr_data():
+
+    assert len(rawRPX_FR.data) == 11555
+
+
+def test_read_raw_rpx_fr_nan_data():
+
+    assert len(rawRPX_FR_NaN.data) == 11581
+
+
+def test_read_raw_rpx_fr_sleep_wake_score():
+
+    assert rawRPX_FR.sleep_wake.sum() == 2260
+
+
+def test_read_raw_rpx_fr_mobility_score():
+
+    assert rawRPX_FR.mobility.sum() == 3104
+
+
+def test_read_raw_rpx_fr_interval_status():
+
+    assert list(rawRPX_FR.interval_status.unique()) == \
+        ['ACTIVITÉ', 'REPOS', 'S-REPOS']
+
+
+def test_read_raw_rpx_ger_name():
+
+    assert rawRPX_GER.name == 'TEST_SAMPLE_GER'
+
+
+def test_read_raw_rpx_ger_uuid():
+
+    assert rawRPX_GER.uuid == 'PXXGER'
+
+
+def test_read_raw_rpx_ger_start_time():
+
+    assert rawRPX_GER.start_time == pd.Timestamp('2020-01-24 09:05:00')
+
+
+def test_read_raw_rpx_ger_frequency():
+
+    assert rawRPX_GER.frequency == pd.Timedelta('00:00:30')
+
+
+def test_read_raw_rpx_ger_data():
+
+    assert len(rawRPX_GER.data) == 20160
+
+
+def test_read_raw_rpx_ger_no_light():
+
+    assert rawRPX_GER.light is None
