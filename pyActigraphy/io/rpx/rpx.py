e112cae622618518fbc913cc1b0490a4fa806ba2
ghammad <gregory.hammad@hotmail.fr>
2022-08-04 15:45:13 +0200

    Adapt light access to use LightRecording methods

--- pyActigraphy/io/rpx/rpx.py
+++ pyActigraphy/io/rpx/rpx.py
@@ -205,7 +205,6 @@ class RawRPX(BaseRaw):
                 data=index_light,
                 frequency=index_light.index.freq
             ) if index_light is not None else None
-            # self.__extract_rpx_data(index_data, 'White_light')
         )
 
     @property
@@ -216,22 +215,22 @@ class RawRPX(BaseRaw):
     @property
     def white_light(self):
         r"""White light levels (in lux.)"""
-        return self.__extract_rpx_data(self.light, "White_light")
+        return self.__extract_light_channel("White_light")
 
     @property
     def red_light(self):
         r"""Red light levels (in microwatt per cm2.)"""
-        return self.__extract_rpx_data(self.light, "Red_light")
+        return self.__extract_light_channel("Red_light")
 
     @property
     def green_light(self):
         r"""Green light levels (in microwatt per cm2.)"""
-        return self.__extract_rpx_data(self.light, "Green_light")
+        return self.__extract_light_channel("Green_light")
 
     @property
     def blue_light(self):
         r"""Blue light levels (in microwatt per cm2.)"""
-        return self.__extract_rpx_data(self.light, "Blue_light")
+        return self.__extract_light_channel("Blue_light")
 
     @property
     def off_wrist(self):
@@ -352,6 +351,12 @@ class RawRPX(BaseRaw):
             return data.loc[:, available_light_cols]
         else:
             return None
+
+    def __extract_light_channel(self, channel):
+        if self.light is None:
+            return None
+        else:
+            return self.light.get_channel(columns[self.language][channel])
 
     def __check_rpx_header(self, fname, cols_available, cols_required):
         if (
