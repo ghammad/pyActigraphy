e3019ab9fd65106ab84f81cbc8b1f135f7abdb71
ghammad <gregory.hammad@hotmail.fr>
2023-02-09 18:47:59 +0100

    Include mean/med/std activity and time elapsed in report

--- pyActigraphy/reports/report_activity.py
+++ pyActigraphy/reports/report_activity.py
@@ -51,8 +51,61 @@ class ActivityReport(Report):
         r'''Result accessor'''
         return self.__results
 
-    def fit(self, verbose=False):
-        r'''DESCRIPTION'''
+    def fit(
+        self,
+        threshold=None,
+        start_time=None,
+        stop_time=None,
+        oformat=None,
+        verbose=False
+    ):
+        r"""Compute time spent above thresholds.
+
+        Calculate the total time spent within the specified activity ranges.
+
+        Parameters
+        ----------
+        threshold: float, optional
+            If not set to None, discard data below threshold before computing
+            activity ranges.
+            Default is None.
+        start_time: str, optional
+            If not set to None, discard data before start time,
+            on a daily basis.
+            Supported time string: 'HH:MM:SS'
+            Default is None.
+        stop_time: str, optional
+            If not set to None, discard data after stop time, on a daily basis.
+            Supported time string: 'HH:MM:SS'
+            Default is None.
+        oformat: str, optional
+            Output format. Available formats: 'minute' or 'timedelta'.
+            If set to 'minute', the result is in number of minutes.
+            If set to 'timedelta', the result is a pd.Timedelta.
+            If set to None, the result is in number of epochs.
+            Default is None.
+        Returns
+        -------
+        tat : pd.Series
+            Report on the time spent within activity ranges.
+        """
+
+        available_formats = [None, 'minute', 'timedelta']
+        if oformat not in available_formats:
+            raise ValueError(
+                'Specified output format ({}) not supported. '.format(oformat)
+                + 'Available formats are: {}'.format(str(available_formats))
+            )
+
+        if oformat == 'minute':
+            scale = self.data.index.freq.delta/pd.Timedelta('1min')
+            time_label = 'Time elapsed (min.)'
+        elif oformat == 'timedelta':
+            scale = self.data.index.freq.delta
+            time_label = 'Time elapsed'
+        else:
+            scale = None
+
         if not self.cut_points:
             print('Set cut points before running the fit method.')
             return None
@@ -76,17 +129,44 @@ class ActivityReport(Report):
                 print('Cut-points are interpreted as activity thresholds.')
 
         # Add min/max activity counts to form boundaries
-        activity_thr = np.concatenate([[-1], activity_thr, [np.infty]])
+        activity_thr = np.concatenate(
+            [[-np.infty], activity_thr, [np.infty]]
+        )
+
+        if threshold is not None:
+            data_mask = self.data.mask(self.data < threshold)
+        else:
+            data_mask = self.data
+
+        if start_time is stop_time is None:
+            data_mask_in = data_mask
+        elif (start_time is None) or (stop_time is None):
+            raise ValueError(
+                'Both start and stop times have to be specified, if any.'
+            )
+        else:
+            data_mask_in = data_mask.between_time(
+                start_time=start_time, end_time=stop_time, include_end=False
+            )
 
         out, bins = pd.cut(
-            self.data.values,
-            activity_thr,
+            data_mask_in,  # self.data.values,
+            bins=activity_thr,
             labels=self.labels,
             retbins=True,
+            include_lowest=True,
             duplicates='drop'
         )
 
-        self.__results = out.describe()['freqs']
+        results = data_mask_in.groupby(out).agg(
+            ['mean', 'median', 'std', 'count']
+        )
+
+        if scale:
+            results.loc[:, 'count'] *= scale
+            results.rename(columns={'count': time_label}, inplace=True)
+
+        self.__results = results
         # Reset index name
         self.__results.index.name = None
 
