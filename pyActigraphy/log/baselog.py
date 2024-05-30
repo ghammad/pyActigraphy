c08167f5a204d4da7ca7153fdb00bfd734ec7776
ghammad <gregory.hammad@hotmail.fr>
2024-05-30 15:24:01 +0200

    Add DST check in BaseLog

--- pyActigraphy/log/baselog.py
+++ pyActigraphy/log/baselog.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import pyexcel as pxl
+from pytz import timezone
 
 
 class BaseLog():
@@ -13,13 +14,19 @@ class BaseLog():
         Absolute filepath of the input log file.
     log: pandas.DataFrame
         Dataframe containing the data found in the log file.
+    time_zone: str, optional
+        Time zone of time stamps. Ex: 'Europe/Brussels'.
+        If set to None, the time stamps in the log file are considered as tz-naive.
+        In this case, checking for overlaps between date range and DST times is not possible.
+        Default is None.
 
     """
 
     def __init__(
         self,
         input_fname,
-        log
+        log,
+        time_zone=None
     ):
 
         # get absolute file path
@@ -27,6 +34,33 @@ class BaseLog():
 
         # Add `duration` column
         log['Duration'] = log['Stop_time'] - log['Start_time']
+
+        # Add DST check
+        if time_zone is not None:
+            from pytz import timezone
+            tz = timezone(time_zone)
+            # Localize the log timestamps
+            log.loc[:,'Start_time'] = log.Start_time.dt.tz_localize(tz)
+            log.loc[:,'Stop_time'] = log.Stop_time.dt.tz_localize(tz)
+            # compute the DST transition times according to the specified time zone
+            transition_times = [
+                t.replace(tzinfo=timezone('UTC')).astimezone(tz)
+                for t in tz._utc_transition_times[1:]
+            ]
+            # Dictionary mapping the transition times to the corresponding year                                                                                                                                                    
+            transition_times_by_year = {
+                start_time.year: [start_time, stop_time]
+                for start_time, stop_time in zip(
+                    transition_times[::2], transition_times[1::2]
+                    )
+            }
+
+            log.loc[:,'DST_crossover'] = log.apply(
+                __is_straddling_dst_transitions,
+                dst_transition_times_dict=transition_times_by_year,
+                axis=1
+            )
+
 
         # Inplace drop of NA
         log.dropna(inplace=True)
@@ -160,6 +194,84 @@ class BaseLog():
         log.index.name = index_name
 
         return log
+
+    @staticmethod
+    def __is_straddling_dst_transitions(times, dst_transition_times_dict):
+        """ Verify if the date range includes a DST transition time
+
+        Function designed to be applied to 'log' dataframe in order to check
+        if the date range between the specified start and stop times
+        do include a DST (either CET->CEST or CEST->CET) transition.
+
+        Parameters
+        ----------
+        times: pd.Series
+            Series containing the following indices: 'Start_time' and 'Stop_time'.
+        dst_transition_times_dict: dict
+            Dictionary with years as keys and the corresponding DST transition datetimes as values (2-tuple).
+
+        Returns
+        -------
+        dst: bool
+            True if the date range straddles a DST. False otherwise.
+
+        """
+    
+        # Extract the year of the start AND stop times
+        year_start = times['Start_time'].year
+        year_stop = times['Stop_time'].year
+        
+        # Check if start-stop range spans over new year's eve
+        span_nye = (year_start!=year_stop)
+        
+        # Shortcut: if the recording is longer than a year, then
+        # it has crossed DST transition times at least once.
+        # NB: actually it is already the case for 7-month long recordings
+        if (times['Stop_time']-times['Start_time']) >= pd.Timedelta('365D'):
+            return True
+        
+        if not span_nye:
+            # Simple case: 
+            # the date range should only be tested with DST
+            # transition times of the current (i.e start) year
+            cet2cest, cest2cet = dst_transition_times_dict[year_start]
+
+            # Does the date range contain CET->CEST:
+            isCET2CEST = (times['Start_time'] <= cet2cest <= times['Stop_time'])
+
+            # Does the date range contain CEST->CET:
+            isCEST2CET = (times['Start_time'] <= cest2cet <= times['Stop_time'])
+
+            return (isCET2CEST or isCEST2CET)
+        else:
+            # Not so simple case:
+            # For (very) long recordings, spanning a New Year's Eve,
+            # the date range should also be tested with DST transition times
+            # of the next year (year stop)
+            cet2cest_start, cest2cet_start = transition_times_by_year[year_start]
+            cet2cest_stop, _ = transition_times_by_year[year_stop]
+
+            # Does the date range contain CET->CEST (Year=N):
+            isCET2CEST_start = (times['Start_time'] <= cet2cest_start <= times['Stop_time'])
+
+            # Does the date range contain CEST->CET (Year=N):
+            isCEST2CET_start = (times['Start_time'] <= cest2cet_start <= times['Stop_time'])
+
+            # Does the date range contain CET->CEST (Year=N+1):
+            isCET2CEST_stop = (times['Start_time'] <= cet2cest_stop <= times['Stop_time'])
+
+            return (isCET2CEST_start or isCEST2CET_start or isCET2CEST_stop)
+
+            year = times['Start_time'].year
+            cet2cest, cest2cet = transition_times_by_year[year]
+            
+            # Does the date range contain CET->CEST:
+            isCET2CEST = (times['Start_time'] <= cet2cest <= times['Stop_time'])
+            
+            # Does the date range contain CEST->CET:
+            isCEST2CET = (times['Start_time'] <= cest2cet <= times['Stop_time'])
+            
+            return (isCET2CEST or isCEST2CET)        
 
     @property
     def fname(self):
